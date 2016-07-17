0bfcb973c2196b7b175825c28e6762772ca827e9
Nick Doiron <ndoiron@mapmeld.com>
2016-07-17 18:46:09 -0400

    got it working somewhat

--- server.py
+++ server.py
@@ -9,8 +9,10 @@ app = flask.Flask(__name__)
 
 # colorado geo fence
 # xmin, ymin, xmax, ymax // west, south, east, north
-# * 1000
-geobox = [102052, 36971, 109190, 41.01]
+# deg * 1000 units
+# add 90 deg to north/south
+# add 180 deg to east/west
+geobox = [-102052 + 180000, 36971 + 90000, -109190 + 180000, 41010 + 90000]
 
 @app.route('/')
 def print_index():
@@ -21,36 +23,28 @@ def calculate_geo():
     # rebuild public key
     g = int(request.args.get('g'))
     n = int(request.args.get('n'))
-    print('got pubkey 1')
     pubkey = paillier.PaillierPublicKey(g=g, n=n)
-    print('got pubkey 2')
 
-    lat = paillier.EncryptedNumber(pubkey, int(request.args.get('lat')), 1)
-    lng = paillier.EncryptedNumber(pubkey, int(request.args.get('lng')), 1)
-    print('got lat/lng')
+    # load the coordinates as EncryptedNumber type
+    lat = paillier.EncryptedNumber(pubkey, int(request.args.get('lat')))
+    lng = paillier.EncryptedNumber(pubkey, int(request.args.get('lng')))
 
+    # use same key to encrypt the geobox
     west = pubkey.encrypt(geobox[0])
     south = pubkey.encrypt(geobox[1])
     east = pubkey.encrypt(geobox[2])
     north = pubkey.encrypt(geobox[3])
-    print ('got geo box')
 
     latoffset = (south - lat)
     latoffset2 = (north - lat)
     lngoffset = (west - lng)
     lngoffset2 = (east - lng)
 
-    print ('made offsets')
-
     return json.dumps({
-      "lat": latoffset.ciphertext(),
-      "latex": latoffset.exponent,
-      "lat2": latoffset2.ciphertext(),
-      "lat2ex": latoffset2.exponent,
-      "lng": lngoffset.ciphertext(),
-      "lngex": lngoffset.exponent,
-      "lng2": lngoffset2.ciphertext(),
-      "lng2ex": lngoffset2.exponent
+      "lat": str(latoffset.ciphertext()),
+      "lat2": str(latoffset2.ciphertext()),
+      "lng": str(lngoffset.ciphertext()),
+      "lng2": str(lngoffset2.ciphertext()),
     })
 
 if __name__ == '__main__':
--- static/client-ui.js
+++ static/client-ui.js
@@ -5,10 +5,10 @@ $('button').prop('disabled', false).click(function(e) {
   if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(function(position) {
       var latitude = Math.round(position.coords.latitude * 1000);
-      var longitude = Math.round(position.coords.longitude * -1000);
-      $('#clientgeo').text((latitude / 1000) + ', ' + (longitude / -1000));
-      latitude = kp.public_key.raw_encrypt(latitude + '');
-      longitude = kp.public_key.raw_encrypt(longitude + '');
+      var longitude = Math.round(position.coords.longitude * 1000);
+      $('#clientgeo').text((latitude / 1000) + ', ' + (longitude / 1000));
+      latitude = kp.public_key.raw_encrypt((latitude + 90000) + '');
+      longitude = kp.public_key.raw_encrypt((longitude + 180000) + '');
 
       $('#encryptlat').text(latitude);
       $('#encryptlng').text(longitude);
@@ -19,7 +19,28 @@ $('button').prop('disabled', false).click(function(e) {
         lat: latitude.toString(),
         lng: longitude.toString()
       }, function (response) {
-        console.log(response);
+        response = JSON.parse(response);
+        $('#response').text(JSON.stringify(response));
+
+        var lat1 = kp.private_key.raw_decrypt(response.lat);
+        var lat2 = kp.private_key.raw_decrypt(response.lat2);
+        var lng1 = kp.private_key.raw_decrypt(response.lng);
+        var lng2 = kp.private_key.raw_decrypt(response.lng2);
+
+        var ymatch = (Math.log(lat1) > 50 && Math.log(lat2) < 50) || (Math.log(lat1) < 50 && Math.log(lat2 > 50));
+        var xmatch = (Math.log(lng1) > 50 && Math.log(lng2) < 50) || (Math.log(lng1) < 50 && Math.log(lng2 > 50));
+
+        var gotit = '';
+        if (xmatch && ymatch) {
+          gotit = "<h1>You're in Colorado!</h1>";
+        } else if (xmatch) {
+          gotit = '<strong>Inside the longitude bounds, but not latitude</strong>';
+        } else if (ymatch) {
+          gotit = '<strong>Inside the latitude bounds, but not longitude</strong>';
+        } else {
+          gotit = '<strong>Not in lat or lng bounds :( </strong>';
+        }
+        $('#result').html(gotit);
       });
     },
     function (error) {
